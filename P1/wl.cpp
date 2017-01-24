225c10603866eebd97e52424744a7b13fb6ee777
Karan Talreja <karantalreja91@gmail.com>
2017-01-24 16:13:48 -0600

    Vector based implementation

--- P1/wl.cpp
+++ P1/wl.cpp
@@ -1,13 +1,115 @@
 //
-// File: wl.h
+// File: wl.cpp
 // 
-//  Description: Add stuff here ... 
-//  Student Name: Add stuff here .. 
-//  UW Campus ID: Add stuff here .. 
-//  enamil: Add stuff here .. 
+//  Description: This file contains the implementation of word locator. 
+//  Student Name: Karan Talreja
+//  UW Campus ID: 9075678186
+//  enamil: talreja2@wisc.edu 
 
+#include "wl.h"
 
-int main()
+/**
+ * @brief main function
+ * @param argc Input number of arguments
+ * @param argv Input arguments
+ */
+int main(int argc, char** argv)
 {
-   return 0;
+  char command[514];
+  char* currCommand = command, *token = command;
+  FILE* fin = stdin, *currLoadFile = NULL;
+  char* rc = NULL;
+  command_e cmd = ERROR_e;
+  createDelim();
+  while (1) {
+    cmd = ERROR_e;
+    //wl_printf(">");
+    rc = fgets(command, sizeof(command), fin);
+    if (rc == NULL) continue;
+
+    char* context;
+    char* argvs[4];
+    int i = 0;
+    currCommand = command;
+    rc = strchr(currCommand, '\n');
+    if (NULL != rc) *rc = '\0';
+
+    while (NULL != (token = strtok_r(currCommand, " ", &context))) {
+      currCommand = NULL;
+      argvs[i++] = token;
+      if (i > 3) break;
+    }
+    
+    if (i == 1) {
+      if (0 == strcasecmp(argvs[0], "new")) cmd = NEW_e;
+      else if (0 == strcasecmp(argvs[0], "end")) cmd = END_e;
+    } else if (i == 2) {
+      if (0 == strcasecmp(argvs[0], "load")) cmd = LOAD_e;
+    } else if (i == 3) {
+      if (0 == strcasecmp(argvs[0], "locate")) cmd = LOCATE_e;
+    } else {
+        wl_printf("ERROR: Invalid command\n");
+        continue;
+    }
+    
+    switch (cmd) {
+      case LOAD_e: {
+        currLoadFile = fopen(argvs[1], "r");
+        if (NULL == currLoadFile) {
+          wl_printf("ERROR: Invalid command\n");
+          continue;
+        }
+        if (NULL != root) {
+          delete root;
+          root = NULL;
+        }
+        char line[100];
+        int idx = 1;
+        while (NULL != fgets(line, sizeof(line), currLoadFile)) {
+          currCommand = line;
+          while (NULL != (token = strtok_r(currCommand, delim.c_str(), &context))) {
+            currCommand = NULL;
+            std::string temp(token);
+            root = insert(root, temp.c_str(), idx);
+            idx++;
+          }
+        }
+        fclose(currLoadFile);
+        //inorder(root);
+      }
+        break;
+      case LOCATE_e: {
+        int occurence = atoi(argvs[2]);
+        if (occurence < 0) {
+          wl_printf("ERROR: Invalid command\n");
+          continue;
+        }
+        if (false == checkCorrectChars(argvs[1])) {
+          wl_printf("ERROR: Invalid command\n");
+          continue;
+        }
+        std::string temp(argvs[1]);
+        node* rc = lookup(root, temp.c_str());
+        if (rc == NULL) {
+          wl_printf("No matching entry\n");
+        } else if (rc->index.size() < (unsigned int)occurence) {
+          wl_printf("No matching entry\n");
+        } else {
+          wl_printf("%d\n",rc->index[occurence - 1]);
+        }
+      }
+        break;
+      case NEW_e:
+        delete root;
+        root = NULL;
+        break;
+      case END_e:
+        return 0;
+        break;
+      default:
+        wl_printf("ERROR: Invalid command\n");
+        continue;
+    }
+  }
+  return 0;
 }
--- P1/wl.h
+++ P1/wl.h
@@ -1,8 +1,145 @@
 //
 // File: wl.h
 // 
-//  Description: Add stuff here ... 
-//  Student Name: Add stuff here .. 
-//  UW Campus ID: Add stuff here .. 
-//  enamil: Add stuff here .. 
+//  Description: This file contains the data structure for word locator. 
+//  Student Name: Karan Talreja
+//  UW Campus ID: 9075678186
+//  enamil: talreja2@wisc.edu
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
 
+///@brief Type to specify the type of input command.
+typedef enum _command {
+  LOAD_e,     ///< "load" command
+  LOCATE_e,   ///< "locate" command
+  NEW_e,      ///< "new" command
+  END_e,      ///< "end" command
+  ERROR_e     ///< Default error type
+} command_e;
+
+std::string delim;
+char output[1000] = {0};
+
+///@brief Helper macro to print to stdout using write system call.
+#define wl_printf(format, ...) \
+  sprintf(output, format, ##__VA_ARGS__); \
+  if (write(STDOUT_FILENO, output, strlen(output)) == -1) \
+    perror("Error in writing to STDOUT\n");
+
+/**
+ * @brief Function to create a deliminator string to be used for tokenizing input file via strtok_r.
+ * @return Nothing
+ */
+void createDelim() {
+  for(int i = 1; i < 256; i++) {
+    if (i >= 48 && i <= 57) continue;   //[0-9] cannot end word
+    if (i >= 65 && i <= 90) continue;   //[A-Z] cannot end word
+    if (i >= 97 && i <= 122) continue;  //[a-z] cannot end word
+    if (i == 39) continue;              //'(Apostrophe) cannot end word
+    delim += (char)i;
+  }
+}
+
+bool checkCorrectChars(const char* word) {
+  for (; *word != '\0'; word++) {
+    int i = *word;
+    if (i >= 48 && i <= 57) continue;         //[0-9] valid 
+    else if (i >= 65 && i <= 90) continue;    //[A-Z] valid 
+    else if (i >= 97 && i <= 122) continue;   //[a-z] valid 
+    else if (i == 39) continue;               //'(Apostrophe) cannot end word
+    else if (i == 0) continue;
+    else return false;
+  }
+  return true;
+}
+
+/**
+ * @brief Class to represent the node of a binary search tree.
+ */
+class node {
+  public:
+    node* left;               ///< Member Pointer to left subtree of this node.
+    node* right;              ///< Member Pointer to right subtree of this node.
+    char* word;               ///< Member for Word stored in this node.
+    std::vector<int> index;   ///< Indices of this word in the input document
+
+    ///@brief Default constructor which sets all member pointers to NULL
+    node():left(NULL), right(NULL), word(NULL) {}
+
+    /**
+     * @brief One argument constructor with input argument to set word.
+     * @param word  Input word to be stored in this node.
+     */
+    node(char* word):left(NULL), right(NULL), word(word) {}
+    
+    ///@brief Destructor for memory deallocation.
+    ~node() {
+      if (NULL != word) free(word);
+      if (NULL != left) delete left;
+      if (NULL != right) delete right;
+    }
+} *root;
+	
+/**
+ * @brief Function to insert a word into binary search tree.
+ * @param	word	word from the document which is to be inserted into the tree.
+ * @param index Index at which this word was found in the document.
+ * @return The new root of the binary search tree.
+ */
+node* insert(node* root, const char* word, int index) {
+  if (root == NULL) {
+    root = new node(strdup(word));
+    root->index.push_back(index);
+    return root;
+  }
+  int rc = strcasecmp(word, root->word);
+  if (rc < 0) {
+    node* retNode = insert(root->left, word, index);
+    if (root->left == NULL) root->left = retNode;
+  } else if (rc > 0) {
+    node* retNode = insert(root->right, word, index);
+    if (root->right == NULL) root->right = retNode;
+  } else root->index.push_back(index);
+  return root;
+}
+
+/**
+ * @brief Function to lookup a word from the binary search tree.
+ * @param	root	The root of the binary search tree in which the node is to be looked up.
+ * @param word  The word which is to be looked for
+ * @details The function returns if the input is NULL. If not it checks if the input word
+ *          is equal to, less than or greater than the current node's word. If it's equal
+ *          current node is returned. Otherwise the word is lookedup into left subtree or
+ *          right sub tree.
+ * @return The node which contains the information for the query word. If it doesn't exist
+ *         NULL is returned.
+ */
+node* lookup(node* root, const char* word) {
+  if (root == NULL) return NULL;
+  node* retNode = NULL;
+  int rc = strcasecmp(word, root->word);
+  if (rc < 0) retNode = lookup(root->left, word);
+  else if (rc > 0) retNode = lookup(root->right, word);
+  else retNode = root;
+  return retNode;
+}
+
+/**
+ * @brief Function to do a inorder traversal on the binary search tree.
+ * @param	root	The root of the binary search tree on which in order traversal is to be done.
+ * @return Nothing.
+ */
+void inorder(node* root) {
+  if (root == NULL) return;
+  inorder(root->left);
+  wl_printf("%s ",root->word);
+  for (std::vector<int>::iterator itr = root->index.begin(); itr != root->index.end(); itr++) {
+    wl_printf("%d ",*itr);
+  }
+  wl_printf("\n");
+  inorder(root->right);
+}

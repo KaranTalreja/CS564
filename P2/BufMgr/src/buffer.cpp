a61276f8353a091ebc351f651600601072e6b2c6
Karan Talreja <karantalreja91@gmail.com>
2017-02-01 21:15:42 -0600

    Passed all tests given in the default driver

--- P2/BufMgr/src/buffer.cpp
+++ P2/BufMgr/src/buffer.cpp
@@ -40,29 +40,86 @@ BufMgr::~BufMgr() {
 
 void BufMgr::advanceClock()
 {
-
+  clockHand = (clockHand + 1) % numBufs;
 }
 
-void BufMgr::allocBuf(FrameId & frame) 
+void BufMgr::allocBuf(FrameId & frame)
 {
+  unsigned int cnt = 0;
+  while(1) {
+    advanceClock();
+    if (cnt == numBufs) throw BufferExceededException();
+    if (bufDescTable[clockHand].pinCnt == 0 && bufDescTable[clockHand].refbit == false) {
+      frame = clockHand;
+      break;
+    } else if (bufDescTable[clockHand].pinCnt == 1) cnt++;
+    bufDescTable[clockHand].refbit = false;
+  }
+  if (bufDescTable[frame].valid == true) {
+    hashTable->remove(bufDescTable[frame].file, bufDescTable[frame].pageNo);
+    if (bufDescTable[frame].dirty == true) bufDescTable[frame].file->writePage(bufPool[frame]);
+    bufDescTable[frame].Clear();
+  }
+  return;
 }
 
-	
 void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
 {
+  FrameId frameNo;
+  bool rc = hashTable->lookup(file, pageNo, frameNo);
+  if (rc == false) {
+    allocBuf(frameNo);
+    Page pageRc = file->readPage(pageNo);
+    bufPool[frameNo] = pageRc;
+    page = &bufPool[frameNo];
+    hashTable->insert(file, pageNo, frameNo);
+    bufDescTable[frameNo].Set(file, pageNo);
+  } else {
+    page = &bufPool[frameNo];
+    bufDescTable[frameNo].refbit = 1;
+    bufDescTable[frameNo].pinCnt++;
+  }
+  return;
 }
-
 
 void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty) 
 {
+  FrameId frameNo;
+  bool rc = hashTable->lookup(file, pageNo, frameNo);
+  if (rc == false) return;
+  if (bufDescTable[frameNo].pinCnt == 0) throw PageNotPinnedException(file->filename(), pageNo, frameNo);
+  bufDescTable[frameNo].pinCnt--;
+  if (dirty == true) bufDescTable[frameNo].dirty = true;
 }
 
 void BufMgr::flushFile(const File* file) 
 {
+  for (unsigned int i = 0; i < numBufs; i++) {
+    if (bufDescTable[i].file == file) {
+      if (bufDescTable[i].pageNo == Page::INVALID_NUMBER)
+        throw BadBufferException(i, bufDescTable[i].dirty, bufDescTable[i].valid, bufDescTable[i].refbit);
+      if (bufDescTable[i].pinCnt > 0) throw PagePinnedException(file->filename(), bufDescTable[i].pageNo, i);
+      if (bufDescTable[i].dirty == true) {
+        bufDescTable[i].file->writePage(bufPool[i]);
+        bufDescTable[i].dirty = false;
+      }
+      hashTable->remove(bufDescTable[i].file, bufDescTable[i].pageNo);
+      bufDescTable[i].Clear();
+    }
+  }
 }
 
 void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page) 
 {
+  Page pageRc = file->allocatePage();
+  FrameId frameNo;
+  allocBuf(frameNo);
+  bufPool[frameNo] = pageRc;
+  page = &bufPool[frameNo];
+  pageNo = page->page_number();
+  hashTable->insert(file, pageNo, frameNo);
+  bufDescTable[frameNo].Set(file, pageNo);
+  return;
 }
 
 void BufMgr::disposePage(File* file, const PageId PageNo)
